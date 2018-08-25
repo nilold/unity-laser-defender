34b06c78c3de71c47e98fc58212328fbf9f7c764
Nilo Serafim Neto <nilold@gmail.com>
2018-08-25 16:50:03 -0300

    6.88 prefabs done

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float movePadding = 1f;
+    [SerializeField] Laser laser;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+	// Use this for initialization
+	void Start () {
+        SetUpMoveBondaries();
+	}
+
+    private void SetUpMoveBondaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + movePadding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - movePadding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + movePadding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - movePadding;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY,yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
