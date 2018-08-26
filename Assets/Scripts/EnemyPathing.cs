9ed3d6a5de285cef03f8ab1368797f3eb0a0d550
nilo <nilold@gmail.com>
2018-08-26 17:29:43 -0300

    added animation to first wave, but lost control over postions

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -32,11 +32,21 @@ public class EnemyPathing : MonoBehaviour
 
     private void Move()
     {
+        //Debug.Log("WaypontIndex: " + waypointIndex.ToString());
+        //Debug.Log("WaypontCount: " + waypoints.Count.ToString());
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].position;
+
+            //Debug.Log("target pos: " + targetPosition.ToString());
+            //Debug.Log("GetMoveSpeed: " + waveConfig.GetMoveSpeed().ToString());
+            //Debug.Log("deltaTime: " + Time.deltaTime.ToString());
+            //Debug.Log("old pos: " + transform.position.ToString());
+
             var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            //Debug.Log("new pos: " + transform.position.ToString());
 
             if (transform.position == targetPosition)
                 waypointIndex++;
