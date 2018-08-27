c5e539ed91d817155f4442959dbc81cf4f527f0c
nilo <nilold@gmail.com>
2018-08-27 20:47:49 -0300

    start menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+            
+    }
+
+    public void LoadPreviousScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex - 1);
+    }
+
+    public void LoadLastScene(){
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadFirstScene(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+}
